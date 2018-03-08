4335fe458d621e0b943eb34da58f1fe4e6abab02
Paulo Carreira <carreira_p@yahoo.com.br>
2018-03-08 07:51:36 -0300

    first commit with login working with a mock service

---
+++ fichasordens/src/app/app.module.ts
@@ -0,0 +1,88 @@
+/*import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+// used to create fake backend
+import { fakeBackendProvider } from './_helpers/index';
+import { MockBackend, MockConnection } from '@angular/http/testing';
+import { BaseRequestOptions } from '@angular/http';
+
+import { AppComponent }  from './app.component';
+
+
+import { AuthGuard } from './_guards/index';
+import { AuthenticationService, UserService } from './_services/index';
+import { LoginComponent } from './login/index';
+import { HomeComponent } from './home/index';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        routing
+    ],
+    declarations: [
+        AppComponent,
+        LoginComponent,
+        HomeComponent
+    ],
+    providers: [
+        AuthGuard,
+        AuthenticationService,
+        UserService,
+
+        // providers used to create fake backend
+        fakeBackendProvider,
+        MockBackend,
+        BaseRequestOptions
+    ],
+    bootstrap: [AppComponent]
+})*/
+
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { routing } from './app.routing';
+
+import { AppComponent } from './app.component';
+import { AuthGuard } from './_guards';
+import { AuthenticationService, UserService } from './_services';
+import { fakeBackendProvider } from './_helpers';
+import { MockBackend, MockConnection } from '@angular/http/testing';
+import { BaseRequestOptions } from '@angular/http';
+import { LoginComponent } from './login/index';
+import { HomeComponent } from './home/index';
+
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routing
+  ],
+  providers: [
+        AuthGuard,
+        AuthenticationService,
+        UserService,
+
+        // providers used to create fake backend
+        fakeBackendProvider,
+        MockBackend,
+        BaseRequestOptions
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ fichasordens/src/app/home/home.component.ts
@@ -0,0 +1,26 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { User } from '../_models/index';
+import { UserService } from '../_services/index';
+import { Router } from '@angular/router';
+
+@Component({
+    moduleId: module.id,
+    templateUrl: 'home.component.html'
+})
+
+export class HomeComponent implements OnInit {
+    users: User[] = [];
+
+    constructor(private userService: UserService, private router: Router) { }
+
+    ngOnInit() {
+        // get users from secure api end point
+        this.userService.getUsers()
+            .subscribe(users => {
+                this.users = users;
+            });
+    }
+
+}
